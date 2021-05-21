bf68fbaa2e38489da968489abe1bbc34edaa3d15
Welinton Jose Diniz Ribeiro <welintonr@ciandt.com>
2021-05-21 16:10:51 -0300

    Assets config

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,8 +14,8 @@ import { PlatformService } from 'src/app/services/platform/platform.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  logoImage = '../../../assets/Logo.svg';
-  authorImage = '../../../assets/User/Photo.png';
+  logoImage = 'assets/Logo.svg';
+  authorImage = 'assets/User/Photo.png';
   valueSearch = '';
   platform: Observable<string>;
   isOpen = false;
